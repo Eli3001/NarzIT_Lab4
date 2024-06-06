9108fe9e0f875298a093a35f6e9201451521078c
Eli <dsw52678@student.dsw.edu.pl>
2024-06-07 00:48:19 +0200

    added error logs

--- skrypt.sh
+++ skrypt.sh
@@ -20,10 +20,11 @@ elif [ "$1" == "--logs" ] || [ "$1" == "-l" ]; then
 
 elif [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
     echo ""
-    echo "skrypt.sh [-d | --date] [-l | --logs] [-h | --help]"
+    echo "skrypt.sh [-d | --date] [-l | --logs] [-h | --help] [-e | --error]"
     echo "--date             Wyswietla obecna date."
     echo "--logs [n=100]     Tworzy n plikow log[n].txt."
     echo "--help             Wyswietla liste instrukcji."
+    echo "--error            Tworzy n plikow error[n].txt."
 
 elif [ "$1" == "--init" ]; then
 
@@ -35,5 +36,20 @@ r=$(basename "$name" .git)
 
 export PATH=$(pwd)/"$name":$PATH
 
+elif [ "$1" == "--error" ] || [ "$1" == "-e" ]; then
+
+    if [ -z "$2" ]; then
+        iters=100
+    else
+        iters=$2
+    fi
+
+    for ((i=1; i<=$iters; i++))
+        do
+            filename="error${i}.txt"
+            echo $filename > $filename
+            echo $0 >> $filename
+            echo $(date) >> $filename
+        done
 fi
 
