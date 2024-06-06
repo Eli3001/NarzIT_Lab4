6680ea7ae87cd15546e01de9d8982918de435010
Eli <dsw52678@student.dsw.edu.pl>
2024-06-06 23:24:40 +0200

    added logging functionality

--- skrypt.sh
+++ skrypt.sh
@@ -2,5 +2,13 @@
 
 if [ "$1" == "--date" ]; then
     echo $(date)
+elif [ "$1" == "--logs" ]; then
+    for i in {1..100}
+        do
+            filename="log${i}.txt"
+            echo $filename > $filename
+            echo $0 >> $filename
+            echo $(date) >> $filename
+        done    
 fi
 
